8a20abce72e554705f40045191e448ef85c06e34
EzraMarin <90418312+EzraMarin@users.noreply.github.com>
2022-03-01 20:59:53 -0700

    finished mission 8

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -35,6 +35,10 @@ namespace Bookstore
 
             services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
             services.AddRazorPages();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +50,7 @@ namespace Bookstore
             }
 
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
